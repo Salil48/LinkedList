0e09c6debbed8e8dc1e32308912eecf2c5de36cf
“Salil48” <salilchharia@gmail.com>
2023-01-11 21:30:00 +0530

    Ordered List

--- Node.cs
+++ Node.cs
@@ -6,16 +6,16 @@ namespace LinkedListOperation
 {
     class Node
     {
-
         //creating Node
         public int data;
         public Node next;
-
 
         public Node(int d)
         {
             data = d;
             next = null;
         }
+
+
     }
 }
